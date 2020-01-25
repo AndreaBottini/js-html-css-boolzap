6545936c2d8fbb220428461ca16d66409ec4c1a7
Andrea Bottini <andrea.bottini22@gmail.com>
2020-01-25 13:55:31 +0100

    Aggiunta prima parte Js e correzione css

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
-// alert('funziona?')
+$(document).ready(function () {
+  $('.icon_send').click(function () {
+    sendMessage()
+  });
+
+});
+
+function sendMessage() {
+  var textMessage = $('input.send_message').val();
+  console.log(textMessage);
+  if(textMessage.length != 0) {
+    var newMessage = $('.template .message').clone();
+    console.log(newMessage);
+
+    newMessage.find('.message_text').text(textMessage);
+    var data = '10:30'
+    newMessage.find('.message_time').text(data);
+    newMessage.addClass('sent');
+    $('.field_chat').append(newMessage);
+    $('input.send_message').val('');
+  }
+}
